3ed3f27c83127ad3e30f4303642f50f7b7b1c272
c1m50c <terminalityms@gmail.com>
2021-11-27 20:00:10 -0600

    Base Layout 🖌

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import NavigationBar from "./components/NavigationBar";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
-
+            <NavigationBar />
         </div>
     );
 }
---
+++ src/components/NavigationBar/index.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function NavigationBar() {
+    return (
+        <nav className="NavigationBar">
+            <h2>Maze Algorithms</h2>
+        </nav>
+    );
+}
+
+export default NavigationBar;
